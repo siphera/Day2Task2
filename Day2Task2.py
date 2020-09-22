83bd371323b5eef635110bf143cbddfafb82125e
siphera <salmansiphenkosi@gmail.com>
2020-09-22 13:39:53 +0200

    day2task2

---
+++ Day2Task2.py
@@ -0,0 +1,16 @@
+#1. Get the largest number from a list
+# Method 1
+input1 = [5, 2, 3, 2 ,4]
+print(max(input1))
+
+# method 2
+input1.sort()
+print(input1[-1])
+
+
+# 2. remove duplicates
+
+input2 = [10, 2, 45, 3, 5, 7, 2, 10, 45, 8, 10]
+print(set(input2))
+
+3.
